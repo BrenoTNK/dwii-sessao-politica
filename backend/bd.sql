272c5343ade37720a573eecaf4a602b7279d8abd
BrenoTNK <brenotanaka3@gmail.com>
2022-11-04 12:21:54 -0300

    primeiro git do trabalho

---
+++ backend/bd.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE sessaoPolitica;
+
+
+USE sessaopolitica;
+
+
+CREATE TABLE politico(
+	idPolitico  INT          PRIMARY KEY AUTO_INCREMENT,
+    usuario     VARCHAR(200) UNIQUE,
+    login       VARCHAR(100) UNIQUE,
+    senha       VARCHAR(80)
+);
+
+CREATE TABLE sessao(
+	idSessao    INT          PRIMARY KEY AUTO_INCREMENT,
+    fkPolitico  INT,
+    FOREIGN KEY (fkPolitico) REFERENCES politico(idPolitico),
+    nome        VARCHAR(150),
+    descricao   VARCHAR(250),
+    tipo        VARCHAR(1),
+    opcao1      VARCHAR(3),
+	opcao2      VARCHAR(3),
+    dataInicial DATE,
+    dataFinal   DATE,
+    qtdMax      INT,
+    qtdVotosPos INT,
+    qtdVotosNeg	INT,
+    estado      VARCHAR(1)
+);
+
+CREATE TABLE voto (
+	fkPolitico  INT,
+	fkSessao    INT, 
+	FOREIGN KEY (fkPolitico) REFERENCES politico(idPolitico),
+	FOREIGN KEY (fkSessao)   REFERENCES sessao(idSessao),
+	resposta    INT,
+	estado      VARCHAR(1)
+);
+
+
+SELECT * FROM politico;
+SELECT * FROM voto;
+SELECT * FROM sessao;
+
+-- Politico de teste
+INSERT INTO politico (usuario, email, senha)
+    VALUES("Seu Joaquim", "seujoaquim45@email.com", "123456");
+
+-- Sessões de teste do politico teste
+INSERT INTO sessao(fkPolitico, nome, descricao, opcao1, opcao2, qtdVotosPos, qtdVotosNeg)
+    VALUES  (1, "Decoração de natal", "Proposta de investir em decoração de Natal no final de ano em toda a cidade", "sim", "nao", 12, 4),
+            (1, "Limpeza do lixo", "Investir e criar campanha de limpeza nas ruas da cidade", "sim", "nao", 23, 15);
+
+
---
+++ backend/index.js
@@ -0,0 +1,125 @@
+// Libs
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const mysql = require("mysql");
+const md5 = require("md5");
+
+// config .env
+require("dotenv").config();
+
+// Server
+const app = express();
+
+try
+{
+    app.listen(process.env.PORT);
+    console.log(`Sistema rodando em http://localhost:${process.env.PORT}`);
+}
+catch (err)
+{
+    console.log(err);
+}
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyparser.urlencoded({ extended: true }))
+
+// Banco de dados
+const db = mysql.createPool({
+    host: process.env.HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+// CRUD
+app.post("/cadastro", (req, res) => {
+    const user = req.body.user;
+    const login = req.body.email;
+    const password = req.body.password;
+
+    const query = `INSERT INTO politico (usuario, login, senha)
+                    VALUES (?,?,md5(?));`;
+    
+    res.send(db.query(query, [user, login, password]));
+});
+
+app.post("/login", (req, res) => {
+    const login = req.body.email;
+    const password = md5(req.body.password);
+
+    const query = `SELECT idPolitico, login, senha FROM politico
+                    WHERE login = (?);`;
+
+    bd.query(query, [login], (err, result) => {
+        const realPass = result[0].senha;
+
+        password == realPass ? res.send({idUser: result[0].idPolitico}) : res.send({idUser: null});
+    });
+});
+
+app.post("/propostas", (req, res) => {
+    const fkPolitico = req.body.idUser;
+
+    const query = `SELECT * FROM sessao
+                    WHERE fkPolitico = (?);`;
+
+    res.send(db.query(query, [fkPolitico]));
+});
+
+///////////////////////////////////////////////////////////////////////
+
+app.post("/listaVotos", (req, res) => {
+    const fkSessao = req.body.fkSessao;
+
+    const query = `SELECT * FROM voto
+                    WHERE fkSessao = (?);`;
+
+    const votos = db.query(query, [fkSessao]);
+    console.log(votos);
+    res.send(votos);
+});
+
+app.post("/votar", (req, res) => {
+    const voto = req.body.voto;
+    const sessao = req.body.sessao;
+    const politico  = req.body.politico;
+
+    const query = `INSERT INTO voto (resposta, fkSessao, fkPolitico,)
+                    VALUES (?,?,?);`;
+
+    res.send(db.query(query, [voto, sessao, politico]));
+});
+
+app.post("/novaSessao", (req, res) => {
+    const nome = req.body.nome;
+    const descricao = req.body.descricao;
+    const tipo = req.body.tipo;
+    const opcao1 = "sim";
+	const opcao2 = "nao";
+    const dataInicial = req.body.dataInicial;
+    const dataFinal = req.body.dataFinal;
+    const qtdMax = req.body.qtdMax;
+    const qtdVotosPos = 0;
+    const qtdVotosNeg = 0;
+    const estado = req.body.estado;
+    const fkPolitico = req.body.fkPolitico;
+
+    const query = `INSERT INTO sessao (nome, descricao, tipo, opcao1, opcao2, dataInicial, dataFinal, qtdMax, qtdVotosPos, qtdVotosNeg, estado, fkPolitico)
+                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?);`;
+
+    res.send(db.query(query, [nome, descricao, tipo, opcao1, opcao2, dataInicial, dataFinal, qtdMax, qtdVotosPos, qtdVotosNeg, estado, fkPolitico]));
+});
+
+app.post("/deletarSessao", (req, res) => {
+    const idSessao = req.body.sessao;
+    const fkPolitico = req.body.politico;
+
+    const query = `DELETE FROM sessao
+                    WHERE idSessao = (?) AND
+                          fkPolitico = (?);`;
+    
+    res.send(db.query(query, [idSessao, fkPolitico]));
+});
