c39379a0940fc9ea5198d2fe46e28c53f6aaa0ee
BrenoTNK <brenotanaka3@gmail.com>
2022-11-05 14:56:29 -0300

    correcao de nome atributo

--- backend/bd.sql
+++ backend/bd.sql
@@ -43,12 +43,10 @@ SELECT * FROM voto;
 SELECT * FROM sessao;
 
 -- Politico de teste
-INSERT INTO politico (usuario, email, senha)
+INSERT INTO politico (usuario, login, senha)
     VALUES("Seu Joaquim", "seujoaquim45@email.com", "123456");
 
 -- Sessões de teste do politico teste
 INSERT INTO sessao(fkPolitico, nome, descricao, opcao1, opcao2, qtdVotosPos, qtdVotosNeg)
     VALUES  (1, "Decoração de natal", "Proposta de investir em decoração de Natal no final de ano em toda a cidade", "sim", "nao", 12, 4),
             (1, "Limpeza do lixo", "Investir e criar campanha de limpeza nas ruas da cidade", "sim", "nao", 23, 15);
-
-
